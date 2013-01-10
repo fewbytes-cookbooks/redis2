8c83231486ed922f4c4ae81405ad024c0a8f7bb9
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-01-10 22:07:04 +0200

    script name too generic

--- recipes/source.rb
+++ recipes/source.rb
@@ -1,22 +1,22 @@
 # This recipe is for compiling redis from source.
 #
 #
-node[:redis][:daemon] = "/usr/local/bin/redis-server"
-directory node[:redis][:build_dir] do
-  owner node[:redis][:build_user]
+node["redis2"]["daemon"] = "/usr/local/bin/redis-server"
+directory node["redis2"]["build_dir"] do
+  owner node["redis2"]["build_user"]
   mode "0755"
   recursive true
 end
 
-remote_file ::File.join(node[:redis][:build_dir], ::File.basename(node[:redis][:source_url])) do
-  source node[:redis][:source_url]
+remote_file ::File.join(node["redis2"]["build_dir"], ::File.basename(node["redis2"]["source_url"])) do
+  source node["redis2"]["source_url"]
   mode "0644"
 end
 
-url = node[:redis][:source_url]
+url = node["redis2"]["source_url"]
 tarball = url.split("?").first.split("/").last
-script "unpack and make" do
-  cwd node[:redis][:build_dir]
+script "unpack and make redis" do
+  cwd node["redis2"]["build_dir"]
   code <<EOS
   wget -O #{tarball} #{url}
   tar -xzf #{tarball}
@@ -25,5 +25,6 @@ script "unpack and make" do
   make install
 EOS
   interpreter "bash"
+  creates node["redis2"]["daemon"]
 end
 
