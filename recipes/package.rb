yieldbot/redis2-cookbook

resolve attribute priority warning

recipes/package.rb
@@ -4,4 +4,4 @@ pkg = value_for_platform( [:ubuntu, :debian] => {:default => "redis-server"},
                          [:centos, :redhat] => {:default => "redis"},
                          :default => "redis")
 package pkg
-node["redis2"]["daemon"] = "/usr/bin/redis-server"
+node.normal["redis2"]["daemon"] = "/usr/bin/redis-server"

fewbytes-cookbooks/redis2

recipes/package.rb
@@ -4,4 +4,4 @@ pkg = value_for_platform( [:ubuntu, :debian] => {:default => "redis-server"},
                          [:centos, :redhat] => {:default => "redis"},
                          :default => "redis")
 package pkg
-node[:redis][:daemon] = "/usr/bin/redis-server"
+node.default["redis2"]["daemon"] = "/usr/bin/redis-server"