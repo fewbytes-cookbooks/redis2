2add03ba45ad1acee66cc1bc23321f22fb99233b
Avishai Ish-Shalom <avishai@fewbytes.com>
2014-03-01 17:14:36 +0200

    Add name attribute to metadata

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "avishai@fewbytes.com"
 license          "Apache 2.0"
 description      "Installs/Configures redis"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+name             "redis2"
 version          "0.5.0"
 
 supports         "ubuntu", ">= 9.04"
