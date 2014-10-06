36bfe90c4a8d224a1a7279aa360b60ed74ed4eb5
Avishai Ish-Shalom <avishai@fewbytes.com>
2014-10-06 20:12:56 +0300

    Fixed #3 search paths for redis-server binary

--- recipes/package.rb
+++ recipes/package.rb
@@ -4,16 +4,16 @@ pkg = value_for_platform( [:ubuntu, :debian] => {:default => "redis-server"},
                          [:centos, :redhat] => {:default => "redis"},
                          :default => "redis")
 package pkg
-node.default["redis2"]["daemon"] = "/usr/bin/redis-server"
 
 # disable service from package
 service "redis-server" do
   action [:disable, :stop]
 end
 
-ruby_block "set redis version" do
+ruby_block "set redis path and version" do
   extend Chef::Mixin::ShellOut
   block do
+    node.default["redis2"]["daemon"] = node["redis2"]["package"]["paths"].find { |path| File.exists? path } unless node["redis2"]["daemon"]
     node.default["redis2"]["version"] = shell_out("#{node["redis2"]["daemon"]} -v").stdout[/(version |v=)([0-9.]+)/,2]
   end
 end
